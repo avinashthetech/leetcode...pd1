5cb2f7d0599ab90fcf182094c1a5e9ba5550c9b5
Avinash <101344389+avinashpdpd@users.noreply.github.com>
2022-09-07 00:30:47 +0530

    Time: 56 ms (26.86%), Space: 14 MB (13.35%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.py
@@ -5,17 +5,30 @@
 #         self.left = left
 #         self.right = right
 #inoder...---> left root right....root is print the val of node
+#st=[]
+#ds=[4,2,5,1,6,3,7]
 class Solution:
     def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        st=[]
+        ds=[]
+        while root is not None or st!=[]:
+            while root is not None:
+                st.append(root)
+                root=root.left
+            root=st.pop()
+            ds.append(root.val)
+            root=root.right
+        return ds
+        
+        
+        
+        """
         ds=[]
       
         if root is None:
             return ds
         return self.inorderTraversal(root.left)+ [root.val]+self.inorderTraversal(root.right)
        
-        
-        
-        """
         if root is None:
             return []
         return self.inorderTraversal(root.left)+[root.val]+self.inorderTraversal(root.right)
