cd7a2fb612aaf96119eb575ae5bacda7dba7c349
Avinash <101344389+avinashpdpd@users.noreply.github.com>
2022-09-07 02:29:52 +0530

    Time: 74 ms (5.38%), Space: 13.9 MB (65.93%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -5,24 +5,20 @@
 #         self.left = left
 #         self.right = right
 #pre order-->root left right
-class Solution:
-    def __init__(self):
-        self.value = 0
+class Solution: 
     
-    def getSums(self,node,path_sum):
+    def __init__(self):
+        self.value=0
+    def getsum(self,node,ps):
+        if node.left is None and node.right is None:
+            self.value+=ps
+        if node.left is not None:
+            self.getsum(node.left,ps*10+node.left.val)
+        if node.right is not None:
+            self.getsum(node.right,ps*10+node.right.val)
             
-            if node.left is None and node.right is None: # found a leaf
-                self.value += path_sum
-            
-            if node.left is not None: # update left child sum 
-                self.getSums(node.left,path_sum*10+node.left.val)
-            
-            if node.right is not None: # update right child sum
-                self.getSums(node.right,path_sum*10+node.right.val)
-
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        
-        self.getSums(root,root.val)
+        self.getsum(root,root.val)
         return self.value
             
             
