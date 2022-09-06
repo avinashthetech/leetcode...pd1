7b1ba5056e76bd98b9076e80fc192eb38211d71e
Avinash <101344389+avinashpdpd@users.noreply.github.com>
2022-09-07 01:42:55 +0530

    Time: 64 ms (14.78%), Space: 13.9 MB (69.11%) - LeetHub

--- 814-binary-tree-pruning/814-binary-tree-pruning.py
+++ 814-binary-tree-pruning/814-binary-tree-pruning.py
@@ -4,6 +4,7 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+#postord-->left right root
 class Solution:
     def pruneTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         """ if not root: return None
