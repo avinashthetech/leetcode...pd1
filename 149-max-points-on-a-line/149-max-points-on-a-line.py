679bac717af91a12ef67fd7babb3e72895110c62
Avinash <101344389+avinashpdpd@users.noreply.github.com>
2022-09-20 10:10:32 +0530

    Time: 102 ms (86.99%), Space: 13.8 MB (92.49%) - LeetHub

---
+++ 149-max-points-on-a-line/149-max-points-on-a-line.py
@@ -0,0 +1,43 @@
+class Solution:
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) == 1: return 1
+        
+        res = 0
+        for p1 in points:
+            m = collections.defaultdict(lambda: 0)
+            for p2 in points:
+                if p1 == p2: continue
+                if p2[0] == p1[0]:
+                    m['na'] += 1
+                    continue 
+                slope = (p2[1] - p1[1]) / (p2[0] - p1[0])
+                m[slope] += 1
+            res = max(res, max(m.values()) + 1)        
+            
+        return res
+        
+        
+        
+        
+        '''
+        def xyz(p1,p2):
+            x1,y1=p1
+            x2,y2=p2
+            if x1==x2:
+                return 'inf'
+            else:
+                return (y2-y1)//(x2-x1)
+        
+        res=0
+        n=len(points)
+        if n<=2:
+            return n
+        for i in range(n):
+            cnt=collections.defaultdict((lambda: 0))
+        for j in range(i+1,n):
+            slope=xyz(points[i],points[i+1])
+            cnt[slope]+=1
+            res=max(res,cnt[slope])     #res = max(res, max(m.values()) + 1)
+        return res+1
+        '''
+        
