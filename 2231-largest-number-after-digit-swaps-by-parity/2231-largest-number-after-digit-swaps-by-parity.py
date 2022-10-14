337a8c41fa3bf71346a659b30044503de68618e8
Avinash <101344389+avinashpdpd@users.noreply.github.com>
2022-10-15 03:27:10 +0530

    Time: 65 ms (19.61%), Space: 14 MB (24.44%) - LeetHub

---
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.py
@@ -0,0 +1,52 @@
+class Solution:
+    def largestInteger(self, num: int) -> int:
+        
+        
+        
+        
+        odd_x = []
+        even_x = []
+        ans = []
+        for i in str(num):
+            i = int(i)
+            if i % 2:
+                odd_x.append(-i)
+                ans.append(True)
+            else:
+                even_x.append(-i)
+                ans.append(False)
+
+        heapq.heapify(odd_x)
+        heapq.heapify(even_x)
+
+        for i in range(len(ans)):
+            if ans[i]:
+                ans[i] = -heapq.heappop(odd_x)
+            else:
+                ans[i] = -heapq.heappop(even_x)
+
+        return int("".join(str(n) for n in ans))
+        
+        
+        
+        
+#         nums=str(num)
+        
+        
+#         for i in range(len(nums)):
+#             if i%2==0:
+#                 if nums[i]<nums[i+1]:
+#                     p=nums[i]
+#                     nums[i]=nums[i+1]
+#                     nums[i+1]=p
+#             else:
+#                 if nums[i]<nums[i+1]:
+#                     p=nums[i]
+#                     nums[i]=nums[i+1]
+#                     nums[i+1]=p
+                    
+                    
+                    
+#         return int(nums)
+                
+        
