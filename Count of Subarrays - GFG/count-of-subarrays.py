4faf54f17864708e9dc14e480e137d8e1ece9173
Avinash <101344389+avinashpdpd@users.noreply.github.com>
2022-11-18 00:00:17 +0530

    Added solution - LeetHub

---
+++ Count of Subarrays - GFG/count-of-subarrays.py
@@ -0,0 +1,33 @@
+#User function Template for python3
+class Solution:
+
+
+	def countSubarray(self,arr, n, k):
+	    # code here
+	    res,cur=0,0
+	    for i in range(n):
+	        if arr[i]<=k:
+	            cur+=1
+	            res+=cur
+	        else:
+	            cur=0
+	    return n*(n+1)//2-res
+	    # code here
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# Driver code 
+if __name__ == "__main__": 		
+    tc=int(input())
+    while tc > 0:
+        n, k=map(int, input().strip().split())
+        arr=list(map(int, input().strip().split()))
+        ob = Solution()
+        ans=ob.countSubarray(arr, n, k)
+        print(ans)
+        tc=tc-1
+# } Driver Code Ends
